184b8176d34fac232641e6489d258227b9ff94a9
ErikJachym <7ejachym@stpatrick.org>
2016-12-14 14:04:12 -0600

    Add files via upload
    fixed my stupidity?

---
+++ First-Semester-Final-master/Cart.js
@@ -0,0 +1,29 @@
+class Cart{
+  //What is the first part of every class? Type it below.
+constructor(i,q){
+  this.itemList = i;
+  this.itemQuantity = q;
+  this.days = d;
+}
+
+
+  //Type the instance functions below this comment.
+addItem(i,q){
+  itemList.push(i);
+  itemQuantity.push(q);
+}
+totalCart(){
+  var total= 0;
+
+  for(let t=0; t<this.itemList.length;t++);
+total+= (this.itemList[i].price*this.itemQuantity[q];
+}
+  return total;
+
+  subCart(d){
+  let subCart= new cart([],[]);
+
+  for(let i=0;i<this.itemList.length;i)
+    if this.itemList.length= d
+}
+}
--- First-Semester-Final-master/password.js
+++ First-Semester-Final-master/password.js
@@ -1,10 +1,17 @@
 class Password{
   //The first part of the class.
-
-
+constructor(pk,prk){
+  this.publickey= pk;
+  this.privatekey= prk;
+}
 
   //Instance functions below this comment.
-
+validPublicKey(){
+  if (this.publickey.length>8 && this.publickey.length<25){
+    return "true";
+  else return "false";
+  }
+}
 
 
   //Static function below this comment.
